9fac6aae2a4e78b49188a0d57c3e396595c7661a
alexsandridge <Alex Sandridge>
2022-06-27 19:14:08 +0000

    Install Expression Engine 6

---
+++ system/user/config/config.php
@@ -0,0 +1,25 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+$config['site_license_key'] = '';
+// ExpressionEngine Config Items
+// Find more configs and overrides at
+// https://docs.expressionengine.com/latest/general/system-configuration-overrides.html
+
+$config['app_version'] = '6.3.4';
+$config['encryption_key'] = 'bbd336b12adc8d2ffe8d53001f844815c3b2d00a';
+$config['session_crypt_key'] = 'd9236aacd680bd0f8905c870a5a31b0d5278c940';
+$config['database'] = array(
+	'expressionengine' => array(
+		'hostname' => 'localhost',
+		'database' => 'alexsandridge',
+		'username' => 'alex',
+		'password' => 'gUMEZ2eN3y6Jyn',
+		'dbprefix' => 'exp_',
+		'char_set' => 'utf8mb4',
+		'dbcollat' => 'utf8mb4_unicode_ci',
+		'port'     => ''
+	),
+);
+$config['show_ee_news'] = 'y';
+
+// EOF
